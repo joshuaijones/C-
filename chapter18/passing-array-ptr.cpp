f801cfe7345e551370c685f43fae2cf7f7f431a8
Joshua Jones <joshuairwinjones@gmail.com>
2025-11-21 20:45:32 -0700

    Completed Chapter 18

---
+++ chapter18/passing-array-ptr.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+#include <ctime>
+
+using namespace std;
+
+double getAverage(int *arr, int size);
+
+int main()
+{
+    int balance[5] = {1000, 2, 3, 17, 50};
+    double avg;
+
+    avg = getAverage(balance, 5);
+
+    cout << "Average value is: " << avg << endl;
+
+    return 0;
+}
+
+double getAverage(int *arr, int size)
+{
+  int i;
+  int sum = 0;
+  double avg;
+
+  for(i = 0; i < size; i++)
+  {
+    sum += arr[i];
+  }
+  
+  avg = double(sum) / size;
+  
+  return avg;
+}
+
